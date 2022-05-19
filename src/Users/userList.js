f28e6e51628819053245969b551dc4eca58af352
Gülbahar <ggulbaharerol@gmail.com>
2022-05-19 15:14:53 +0300

    User page created

--- src/Users/userList.js
+++ src/Users/userList.js
@@ -1,12 +1,58 @@
 
-import React from 'react'
-import {  StyleSheet, View, Text, TouchableOpacity  } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { FlatList } from 'react-native-gesture-handler';
+import { ListItem } from 'react-native-elements'
 
 const Users = ({ navigation }) => {
+    const [users, getUsers] = useState([]);
 
+    useEffect(() => {
 
-    
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then((data) => {
+
+                getUsers(data);
+
+            })
+
+    }, []);
+    const renderUsers = ({ item }) => {
+        return (
+            <ListItem bottomDivider>
+                <ListItem.Content>
+                    <ListItem.Title>{item.name}</ListItem.Title>
+                    <ListItem.Subtitle>{item.username}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+    return (
+        <View style={styles.container}>
+            {
+                <FlatList
+                    data={users}
+                    renderItem={renderUsers}
+                >
+
+                </FlatList>
+            }
+        </View>
+    )
+
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e6e6fa",
+    }
+})
 
 export default Users
+
+
+
+
